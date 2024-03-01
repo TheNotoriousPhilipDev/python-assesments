e75ac5f3ccc3d5b7cf87bdbadcbb00e8e70cb5ac
jefryerson2003 <111019844+jefryerson2003@users.noreply.github.com>
2024-03-01 17:53:15 -0500

    Update a.py

--- a.py
+++ a.py
@@ -1,31 +1,25 @@
-vowels = 'AEIOU'
-
 def minion_game(string):
-  """
-  This function determines the winner of the Minion Game and their score.
-
-  Args:
-      string: The string to analyze.
-
-  Returns:
-      A string containing the winner's name and score, or "Draw" if there is no winner.
-  """
-  kevin_score = 0
-  stuart_score = 0
-  for i in range(len(string)):
-    if string[i] in vowels:
-      kevin_score += len(string) - i
+    vowel = 'AEIOU'
+    StuartPoints = 0
+    KevinPoints = 0
+    l = 0
+    l = len(string)
+    for i in range(l):
+        if string[i] not in vowel:
+            
+            StuartPoints += l - i
+        else:
+            
+            KevinPoints += l - i
+    if StuartPoints > KevinPoints:
+        print("Stuart", StuartPoints)
+    elif StuartPoints < KevinPoints:
+        print("Kevin", KevinPoints)
     else:
-      stuart_score += len(string) - i
+        print("Draw") 
+    return
+    
 
-  if kevin_score > stuart_score:
-    return "Kevin {}".format(kevin_score)
-  elif kevin_score < stuart_score:
-    return "Stuart {}".format(stuart_score)
-  else:
-    return "Draw"
-
-# Test cases
-print(minion_game("BANANA"))  # Output: Stuart 12
-print(minion_game("KICK"))     # Output: Kevin 3
-print(minion_game("AEIOU"))    # Output: Draw
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
